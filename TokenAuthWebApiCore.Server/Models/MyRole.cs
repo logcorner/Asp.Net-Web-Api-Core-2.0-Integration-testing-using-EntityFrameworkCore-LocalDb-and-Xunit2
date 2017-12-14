540fec4bc3abad305a10f41c03eb773f09f34cd5
TOCANE TECHNOLOGIES <tocane.technologies@gmail.com>
2017-12-14 20:09:18 +0100

    upgrade 2.0

---
+++ TokenAuthWebApiCore.Server/Models/MyRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace TokenAuthWebApiCore.Server.Models
+{
+	public class MyRole : IdentityRole
+	{
+		public string Description { get; set; }
+	}
+}
